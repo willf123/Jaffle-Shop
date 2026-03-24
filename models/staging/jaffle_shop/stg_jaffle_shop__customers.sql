388fccbc2e8d510b40b208822c9cb177e272944f
willf123@yahoo.com <willf123@yahoo.com>
2026-03-24 19:36:45 +0000

    generated models

---
+++ models/staging/jaffle_shop/stg_jaffle_shop__customers.sql
@@ -0,0 +1,23 @@
+with 
+
+source as (
+
+    select * from {{ source('jaffle_shop', 'customers') }}
+
+),
+
+renamed as (
+
+    select
+        customer_id,
+        first_name,
+        last_name,
+        first_order_date,
+        most_recent_order_date,
+        number_of_orders
+
+    from source
+
+)
+
+select * from renamed
